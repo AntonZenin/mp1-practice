f2f872cbec053f020141319da941d18491205928
AntonZenin <zenin.anton.2005@gmail.com>
2023-12-06 13:47:35 +0300

    Fixed second

--- ZeninAA/Practice4/Practice4/Practice4.c
+++ ZeninAA/Practice4/Practice4/Practice4.c
@@ -2,130 +2,99 @@
 #include <string.h>
 
 #define N_SYMBOLS 5
-#define NAME_LEN 20
 #define N_PRODUCTS 12
 
-typedef struct {
-    char barcode[N_SYMBOLS];
-    char name[NAME_LEN];
-    float price;
-    float discount;
-}Product; 
+char* products[N_PRODUCTS] = { "Bread", "Milk", "Cheese", "Butter", "Water", "Buiscuits", "Juice", "Lemonade", "Milk chocolate bar", "Sour cream", "Mars", "Nutella" };
+char* barcodes[N_PRODUCTS] = { "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0012" };
+float prices[N_PRODUCTS] = { 60, 95, 650, 140, 35, 115, 120, 106, 150, 90, 65, 460 };
+float discounts[N_PRODUCTS] = { 30, 0, 0, 20, 0, 0, 50, 0, 40, 0, 0, 10 };
+int receipt[N_PRODUCTS] = { 0 };
+float totalDiscount = 0, finalAmount = 0;
 
-Product scanProduct(Product products[], int size) {
-    char barcode[5];
-    printf("Input barcode: ");
-    scanf("%s", barcode);
+int scan_product(char* barcode);
+void input_barcodes();
+void print_receipt();
+void print_final_Amount();
 
-    for (int i = 0; i < size; i++) {
-        if (strcmp(products[i].barcode, barcode) == 0) {
-            return products[i];
-        }
-    }
-
-    Product emptyProduct;
-    strcpy(emptyProduct.barcode, "0000"); 
-    return emptyProduct; 
-}
-int main() {
-    Product products[N_PRODUCTS] = {
-        {"0001", "Bread", 60.f, 30.f},
-        {"0002", "Milk", 95.f, 0.f},
-        {"0003", "Cheese", 650.f, 0.f},
-        {"0004", "Butter", 140.f, 20.f},
-        {"0005", "Water", 35.f, 0.f},
-        {"0006", "Buiscuits", 115.f, 0.f},
-        {"0007", "Juice", 120.f, 50.f},
-        {"0008", "Lemonade", 106.f, 0.f},
-        {"0009", "Milk chocolate bar", 150.f, 5.f},
-        {"0010", "Sour cream", 90.f, 0.f},
-        {"0011", "Mars", 65.f, 0.f},
-        {"0012", "Nutella", 460.f, 10.f}
-    };
-    int size = sizeof(products) / sizeof(products[0]);
-
-    Product receipt[100]; 
-    int receiptSize = 0; 
-    float totalPrice = 0; 
-    float totalDiscount = 0; 
-    float finalAmount = 0; 
-
-    int choice;
-    do {
-        printf("\n1. Scan product\n");
-        printf("2. Show the description of product\n");
-        printf("3. Add product in receipt\n");
-        printf("4. Make a receipt\n");
-        printf("5. Calculate the amount to be paid\n");
-        printf("0. Exit\n");
-        printf("Input choice: ");
-        scanf("%d", &choice);
-        switch (choice) {
-        case 1: {
-            Product scannedProduct = scanProduct(products, size);
-            if (strcmp(scannedProduct.barcode, "0000") == 0) {
-                printf("Product not found\n");
-                break;
-            }
-            printf("Name: %s\n", scannedProduct.name);
-            printf("Price per unit: %.2f\n", scannedProduct.price);
-            printf("Discount: %.2f%%\n", scannedProduct.discount);
-            break;
-        }
-        case 2: {
-            printf("Input the barcode: ");
-            char barcode[5];
-            scanf("%s", barcode);
-            for (int i = 0; i < size; i++) {
-                if (strcmp(products[i].barcode, barcode) == 0) {
-                    printf("Name: %s\n", products[i].name);
-                    printf("Price per unit: %.2f\n", products[i].price);
-                    printf("Discount: %.2f%%\n", products[i].discount);
-                    break;
-                }
-            }
-            break;
-        }
-        case 3: {
-            Product scannedProduct = scanProduct(products, size);
-            if (strcmp(scannedProduct.barcode, "0000") == 0) {
-                printf("Product not found\n");
-                break;
-            }
-            receipt[receiptSize++] = scannedProduct;
-            printf("The product was added to the receipt\n");
-            break;
-        }
-        case 4: {
-            printf("Your receipt:\n");
-            for (int i = 0; i < receiptSize; i++) {
-                Product product = receipt[i];
-                float productTotal = product.price * (100 - product.discount) / 100;
-                printf("%s - %.2f - 1 - %.2f\n", product.name, product.price, productTotal);
-                totalPrice += productTotal;
-                totalDiscount += product.price - productTotal;
-            }
-            break;
-        }
-        case 5: {
-            finalAmount = totalPrice - totalDiscount;
-            printf("Your amount to pay: %.2f\n", finalAmount);
-            break;
-        }
-        case 0: {
-            printf("Exit\n");
-            break;
-        }
-        default: {
-            printf("Incorrect choice\n");
-            break;
-        }
-    }
-  } while (choice != 0);
+void main()
+{
+    int i = 0;
+    input_barcodes();
+    print_receipt();
+    print_final_Amount();
 
     return 0;
 }
+scan_product(char* codes)
+{
+    int i = 0;
+    for (; i < N_PRODUCTS; i++)
+    {
+        if (strcmp(codes, barcodes[i]) == 0) 
+            return i;
+    }
+    return -1;
+}
 
-        
-        
+void input_barcodes()
+{
+    int choice = 0, quant = 0;
+    do
+    {
+        int l;
+        char code[N_SYMBOLS];
+        printf("Input the barcode, please: \n");
+        scanf("%s", code);
+        printf("Input quantity: ");
+        scanf("%d", &quant); 
+        printf("Do you want to exit the shop? (1 - no, 2 - yes)\n");
+        scanf("%d", &choice);
+        if ((choice != 1) && (choice != 2)) { 
+            printf("Error\n"); 
+            break;
+        }
+        l = scan_product(code); 
+        if (l < 0)
+        {
+            printf("Product not found\n");
+            continue;
+        }
+        receipt[l]+= quant; 
+    } while (choice == 1);
+    return;
+}
+void print_receipt()
+{
+    int i;
+    for (i = 0; i < N_PRODUCTS; i++)
+    {
+        if (receipt[i] != 0)
+        {
+            printf("\nProduct: %s\n", products[i]);
+            printf("Price per unit: %lf. \nDiscount: %lf. \nQuantity: %d. \nTotal amount: %f\n",
+                prices[i], discounts[i], receipt[i], prices[i] * ((100 - discounts[i]) / 100) * receipt[i]);
+        }
+    }
+    return;
+}
+void print_final_Amount()
+{
+    int i;
+    for (i = 0; i < N_PRODUCTS; i++)
+    {
+        if (receipt[i] != 0)
+        {
+            totalDiscount += prices[i] - (prices[i] * ((100 - discounts[i]) / 100) * receipt[i]);
+            finalAmount += prices[i] * receipt[i] * ((100 - discounts[i]) / 100);
+            
+        }
+    }
+    printf("\nYour amount to pay: %.2f\n", finalAmount);
+    printf("We will be glad to see you again.\n");
+    return;
+}
  
+
+
+
+
