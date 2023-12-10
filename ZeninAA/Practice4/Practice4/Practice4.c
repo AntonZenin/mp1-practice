cc7bd3e1d12b90481f1786d3e935f64e23ea3c7f
AntonZenin <zenin.anton.2005@gmail.com>
2023-12-10 18:20:03 +0300

    Decorative_Fix

--- ZeninAA/Practice4/Practice4/Practice4.c
+++ ZeninAA/Practice4/Practice4/Practice4.c
@@ -4,11 +4,20 @@
 #define N_SYMBOLS 5
 #define N_PRODUCTS 12
 
-char* products[N_PRODUCTS] = { "Bread", "Milk", "Cheese", "Butter", "Water", "Buiscuits", "Juice", "Lemonade", "Milk chocolate bar", "Sour cream", "Mars", "Nutella" };
-char* barcodes[N_PRODUCTS] = { "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0012" };
+char* products[N_PRODUCTS] = { "Bread", "Milk", "Cheese", "Butter", "Water", "Buiscuits", 
+                
+                    "Juice", "Lemonade", "Milk chocolate bar", "Sour cream", "Mars", "Nutella" };
+
+char* barcodes[N_PRODUCTS] = { "0001", "0002", "0003", "0004", "0005", "0006", 
+                    
+                    "0007", "0008", "0009", "0010", "0011", "0012" };
+
 float prices[N_PRODUCTS] = { 60, 95, 650, 140, 35, 115, 120, 106, 150, 90, 65, 460 };
+
 float discounts[N_PRODUCTS] = { 30, 0, 0, 20, 0, 0, 50, 0, 40, 0, 0, 10 };
+
 int receipt[N_PRODUCTS] = { 0 };
+
 float totalDiscount = 0, finalAmount = 0;
 
 int scan_product(char* barcode);
@@ -48,10 +57,10 @@ void input_barcodes()
         printf("Input quantity: ");
         scanf("%d", &quant); 
         printf("Do you want to exit the shop? (1 - no, 2 - yes)\n");
-        scanf("%d", &choice);
+        scanf("%d", &choice); 
         if ((choice != 1) && (choice != 2)) { 
-            do {
-                printf("Error\n");
+            do { 
+                printf("Error\n"); 
                 scanf("%d", &choice);
             } while ((choice != 1) && (choice != 2));
         }
